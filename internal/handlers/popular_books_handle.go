5593761f22b6b450d18a859bca2946b37704b135
Luis Calle <luedu1103@gmail.com>
2024-11-10 21:54:14 -0500

    Popular books endpoint

--- internal/handlers/popular_books_handle.go
+++ internal/handlers/popular_books_handle.go
@@ -7,23 +7,13 @@ import (
 	dbtypes "pillar/internal/db/types"
 	dbutils "pillar/internal/db/utils"
 
-	"github.com/doug-martin/goqu/v9"
 	"github.com/go-json-experiment/json"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/julienschmidt/httprouter"
 )
 
 func getUserTopTags(conn *pgxpool.Conn, userID, topLimit int) ([]int, error) {
-	query_str, _, query_err := goqu.
-		Select("*").
-		From("Tag").ToSQL()
-
-	if query_err != nil {
-		log.Println(query_err)
-		return nil, nil
-	}
-
-	rows, err := conn.Query(context.Background(), query_str)
+	rows, err := conn.Query(context.Background(), `SELECT tag_id FROM get_user_top_tags($1, $2)`, userID, topLimit)
 	if err != nil {
 		log.Printf("Error executing query: %v", err)
 		return nil, err
@@ -34,7 +24,6 @@ func getUserTopTags(conn *pgxpool.Conn, userID, topLimit int) ([]int, error) {
 
 	for rows.Next() {
 		var tagID int
-		log.Printf("ALGO:")
 		if err := rows.Scan(&tagID); err != nil {
 			log.Printf("Error scanning row: %v", err)
 			return nil, err
@@ -95,46 +84,15 @@ func sendPopularBooks(w http.ResponseWriter, r *http.Request, _ httprouter.Param
 	defer conn.Release()
 
 	// Retrieve user's top tags
-	// topTags, err := getUserTopTags(conn, req.UserID, 5)
-	// if err != nil {
-	// 	log.Println("Failed to get top tags:", err)
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-
-	query_str, _, query_err := goqu.
-		Select("*").
-		From("Tag").ToSQL()
-
-	if query_err != nil {
-		log.Println(query_err)
-		return
-	}
-
-	rows, err := conn.Query(context.Background(), query_str)
+	topTags, err := getUserTopTags(conn, req.UserID, 5)
 	if err != nil {
-		log.Printf("Error executing query: %v", err)
+		log.Println("Failed to get top tags:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	defer rows.Close()
-
-	var tagIDs []int
-
-	for rows.Next() {
-		var tagID int
-		log.Printf("ALGO:")
-		if err := rows.Scan(&tagID); err != nil {
-			log.Printf("Error scanning row: %v", err)
-			return
-		}
-
-		tagIDs = append(tagIDs, tagID)
-	}
-
-	log.Printf("Retrieved Tags: %v", tagIDs)
 
 	// Retrieve popular books using the top tags
-	books, err := getPopularBooks(conn, req.Page, req.PageSize, tagIDs)
+	books, err := getPopularBooks(conn, req.Page, req.PageSize, topTags)
 	if err != nil {
 		log.Println("Failed to get popular books:", err)
 		w.WriteHeader(http.StatusInternalServerError)
